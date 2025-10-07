7f9524236982a9f64cdebcc733687a2c5d06a421
AnuragNegii <Negianurag99@gmail.com>
2025-10-07 20:06:44 +0530

    added gitignore and genai to the project

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 
+import os
+from google import  genai
 
 def main():
+    GEMINI_API_KEY = os.envroment.get
+    client = genai.Client(api_key=GEMINI_API_KEY)
+
+    print(client)
     print("Hello world")
 
 main()
