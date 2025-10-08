06d8328cc55d4c8e582ddc9f2bfe718008562ac3
AnuragNegii <Negianurag99@gmail.com>
2025-10-08 09:00:49 +0530

    basic prompting can now be done

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # AI-Document-Summarizer
-Upload a PDF and get a concise summary of that.
+To work:
+  1- You first need a gemini api key which you can get by making an account in this sit "https://ai.google.dev/"
+  2- Then You have to make a venv environment and use pip install to install google-genai and python-dotenv
+  3- then  make a .env file and paste your gemini-api-key and start using it.
+  
+
+
+Goal - Upload a PDF and get a concise summary of that.
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-
 import os
-from google import  genai
+from google import genai
+from dotenv import load_dotenv
+
 
 def main():
-    GEMINI_API_KEY = os.envroment.get
+    load_dotenv()
+    GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=GEMINI_API_KEY)
+    prompt = "Explain what is the job of open ai?"
+    response = client.models.generate_content(model="gemini-2.5-pro", contents=prompt)
+    print(response.text)
 
-    print(client)
-    print("Hello world")
 
 main()
