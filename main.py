faac183468b30b68223bf8bd549f2dcc6576f2a1
AnuragNegii <Negianurag99@gmail.com>
2025-10-10 22:21:12 +0530

    It can now summarize document now. IDK WHAT TO ADD TO IT NOW

--- main.py
+++ main.py
@@ -2,53 +2,26 @@ import os
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
+import pathlib
 
 
 def main():
     load_dotenv()
     GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=GEMINI_API_KEY)
-    prompt = """
-Alice, Bob, and Carol each live in a different house on the same street: red, green, and blue.
-The person who lives in the red house owns a cat.
-Bob does not live in the green house.
-Carol owns a dog.
-The green house is to the left of the red house.
-Alice does not own a cat.
-Who lives in each house, and what pet do they own?
-"""
-    thoughts = ""
-    answer = ""
-
-    for chunk in client.models.generate_content_stream(
-        model="gemini-2.5-pro",
-        contents=prompt,
-        config=types.GenerateContentConfig(
-            thinking_config=types.ThinkingConfig(include_thoughts=True)
-        ),
-    ):
-        for part in chunk.candidates[0].content.parts:
-            if not part.text:
-                continue
-            elif part.thought:
-                if not thoughts:
-                    print("Thoughts summary:")
-                print(part.text)
-                thoughts += part.text
-            else:
-                if not answer:
-                    print("Answer:")
-                print(part.text)
-                answer += part.text
-    # response = client.models.generate_content(
-    #     model="gemini-2.5-pro",
-    #     contents=prompt,
-    #     config=types.GenerateContentConfig(
-    #         thinking_config=types.ThinkingConfig(include_thoughts=True)
-    #     ),
-    # )
-    # print(response.text)
-    #
+    file_path = pathlib.Path("C:/Users/LENOVO/Downloads/1.pdf")
+    prompt = "Summarize this document"
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=[
+            types.Part.from_bytes(
+                data=file_path.read_bytes(),
+                mime_type="application/pdf",
+            ),
+            prompt,
+        ],
+    )
+    print(response.text)
 
 
 main()
