a1e471608b769d381ab51349194cb474af93889c
AnuragNegii <negianurag99@gmail.com>
2025-10-10 21:36:24 +0530

    added ai thinking but i think ai is not thinking correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 
 
@@ -7,9 +8,47 @@ def main():
     load_dotenv()
     GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=GEMINI_API_KEY)
-    prompt = "Explain what is the job of open ai?"
-    response = client.models.generate_content(model="gemini-2.5-pro", contents=prompt)
-    print(response.text)
+    prompt = """
+Alice, Bob, and Carol each live in a different house on the same street: red, green, and blue.
+The person who lives in the red house owns a cat.
+Bob does not live in the green house.
+Carol owns a dog.
+The green house is to the left of the red house.
+Alice does not own a cat.
+Who lives in each house, and what pet do they own?
+"""
+    thoughts = ""
+    answer = ""
+
+    for chunk in client.models.generate_content_stream(
+        model="gemini-2.5-pro",
+        contents=prompt,
+        config=types.GenerateContentConfig(
+            thinking_config=types.ThinkingConfig(include_thoughts=True)
+        ),
+    ):
+        for part in chunk.candidates[0].content.parts:
+            if not part.text:
+                continue
+            elif part.thought:
+                if not thoughts:
+                    print("Thoughts summary:")
+                print(part.text)
+                thoughts += part.text
+            else:
+                if not answer:
+                    print("Answer:")
+                print(part.text)
+                answer += part.text
+    # response = client.models.generate_content(
+    #     model="gemini-2.5-pro",
+    #     contents=prompt,
+    #     config=types.GenerateContentConfig(
+    #         thinking_config=types.ThinkingConfig(include_thoughts=True)
+    #     ),
+    # )
+    # print(response.text)
+    #
 
 
 main()
